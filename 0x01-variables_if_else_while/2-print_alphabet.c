42a556fc7cf09d066c27c7b48d2821b109064e6c
Iyanu456 <oyerindei13@gmail.com>
2022-09-10 08:29:03 -0700

    new

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,10 +8,10 @@ int main(void)
 {
 	int x;
 
- 	        for (x = 'a'; x <= 'z'; x++)
-	        {
-	        putchar (x);
-	        }
-	        putchar ('\n');
-		return (0);
+	for (x = 'a'; x <= 'z'; x++)
+	{
+	putchar (x);
+	}
+	putchar ('\n');
+	return (0);
 }
