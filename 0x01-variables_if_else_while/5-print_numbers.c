79d4f24ede006e59d27b424a546548e522ee7b63
Iyanu456 <oyerindei13@gmail.com>
2022-09-12 15:03:56 -0700

    fixed

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints all alphabets
+ * main - prints some numbers
  * Description: prints all the alphabets from a to z
  * Return: 0
  */
@@ -8,7 +8,7 @@ int main(void)
 {
 	int x;
 
-	for (x = 0; x <= 9; x++)
+	for (x = '0'; x <= '9'; x++)
 	{
 	putchar (x);
 	}
