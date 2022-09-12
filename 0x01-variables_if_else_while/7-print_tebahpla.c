78249aaa57a4818e57e3b4d416282c6c4e10b3ab
Iyanu456 <oyerindei13@gmail.com>
2022-09-12 16:37:13 -0700

    new

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints all alphabets
+ * Description: prints all the alphabets from a to z
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'z'; x >= 'a'; x--)
+	{
+	putchar (x);
+	}
+	putchar ('\n');
+	return (0);
+}
