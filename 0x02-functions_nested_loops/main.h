d103c1a37c065f01909b7e52f9350e70be446d34
Iyanu456 <oyerindei13@gmail.com>
2022-09-14 05:25:20 -0700

    fixed putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,9 @@
-#include <unistd.h>
 /**
- * putchar - putchar prototype
+* putchar - putchar prototype
  * Description: outputs a string
  * Return: 0
  */
 int _putchar(char a)
 {
-	return (write(1, &a, 1));
+	return (0);
 }
