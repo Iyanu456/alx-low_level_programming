e7e14d1ba0987b0b37d9adca714569bc3afaea34
Iyanu456 <oyerindei13@gmail.com>
2022-09-14 10:42:06 -0700

    default message

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,13 @@
  * Decription: prints all lower case alphabets
  * Return: null
  */
-void print_alphabet(void)
+void print_alphabet (void)
 {
 	int i;
 
-	for(i = 'a'; i <= 'z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
-		_putchar (i);
+	_putchar (i);
 	}
 	_putchar ('\n');
 	return;
