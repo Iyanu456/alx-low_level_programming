61fac32eabb00014f0b906bc24fae7554ace7800
Iyanu456 <oyerindei13@gmail.com>
2022-09-14 05:27:57 -0700

    fixed putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,9 @@
 /**
-* putchar - putchar prototype
+41;320;0c* putchar - putchar prototype
  * Description: outputs a string
  * Return: 0
  */
-int _putchar(char a)
+int _putchar(char)
 {
 	return (0);
 }
