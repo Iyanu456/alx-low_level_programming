2e9a0db7ba8dafa0146f2f463702bdd871e26f75
Iyanu456 <oyerindei13@gmail.com>
2022-09-14 10:57:11 -0700

    default message

---
+++ 0x02-functions_nested_loops/2-print-alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints all alphabets
+ * Decription: prints all lower case alphabets
+ * Return: null
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		int j;
+
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar (j);
+		}
+	}
+	_putchar ('\n');
+}
