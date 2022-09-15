e0e3389c9466b3af53fd69fa42b025ad650b4812
Iyanu456 <oyerindei13@gmail.com>
2022-09-14 20:34:56 -0700

    default message

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs - computes absolute values
+ * Description - computes absolute values
+ * Return: integer values
+ */
+int _abs(int)
+{
+	int n;
+
+	return (abs(n));
+}
