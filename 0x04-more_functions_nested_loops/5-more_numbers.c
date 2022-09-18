37b802325db7fbae41beb3265246186a9bc630c0
Iyanu456 <oyerindei13@gmail.com>
2022-09-18 15:08:28 -0700

    default message

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,13 +7,14 @@ void more_numbers(void)
 {
 	int a;
 
-	for (a = '0'; a <= '9'; a++)
+	for (a = 0; a <= 9; a++)
 	{
 		int b;
 
-		for (b = "0"; b <= "14"; b++)
+		for (b = 0; b <= 14; b++)
 		{
 			_putchar (b);
+		}
+		_putchar ('\n');
 	}
-	_putchar ('\n');
 }
