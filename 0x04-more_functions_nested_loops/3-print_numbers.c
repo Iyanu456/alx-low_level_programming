c47f13bd6a52f1f1faae2960aca2830f8c8548f8
Iyanu456 <oyerindei13@gmail.com>
2022-09-18 02:10:06 -0700

    new

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _print_numbers - checks for uppercase
+ * Description: checks for uppercase
+ * Return: 1
+ */
+int print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(a);
+	}
+	_,putchar('\n')
+	return (1);
+}
