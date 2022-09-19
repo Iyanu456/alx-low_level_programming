99aa9180e52f35e2977813beba66e40b6c42079f
Iyanu456 <oyerindei13@gmail.com>
2022-09-19 15:41:57 -0700

    default

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,18 +2,27 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * print_rev -reverses a string
+ * rev_string -reverses a string
  * @s: string
  * Description: prints a string to std output
  */
 
 void rev_string(char *s)
 {
-	int n;
+	int n, i;
 
-	for (n = (strlen(s) - 1); n >= 0; n--)
+	for (n = (strlen(s) - 1); n >= (strlen(s) / 2); n--)
 	{
-		s = s[n]
+		for (i = 0; i <= (strlen(s) / 2); i++)
+		{
+			int p1, p2;
+
+			p1 = s[i];
+			p2 = s[n];
+
+			s[i] = p2;
+			s[n] = p1;
+		}
 	}
 	_putchar ('\n');
 }
