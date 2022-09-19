91142ac69830a45ee44f4e9da4d0e203e88909cc
Iyanu456 <oyerindei13@gmail.com>
2022-09-19 08:21:21 -0700

    default

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-0;276;0c0;276;0c * swap_int - rests an integer to 98
+ * swap_int - rests an integer to 98
  * @a: an integer
  * @b: an integer
  * Description: resets a number to 98
