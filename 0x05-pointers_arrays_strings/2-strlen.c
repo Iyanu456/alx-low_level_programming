a773b3072e7c958a66714e34986e109c3fe3fa72
Iyanu456 <oyerindei13@gmail.com>
2022-09-19 08:35:14 -0700

    default

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - rests an integer to 98
+ * @s: integer
+ * Description: resets a number to 98
+ * Return: integer
+ */
+
+int _strlen(char *s)
+{
+	return ((strlen (*s)));
+}
