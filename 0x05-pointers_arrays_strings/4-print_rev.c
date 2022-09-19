2c087f73ab149f89ded6778423007a8f743ecb1a
Iyanu456 <oyerindei13@gmail.com>
2022-09-19 13:49:40 -0700

    default

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev -reverses a string
+ * @s: string
+ * Description: prints a string to std output
+ */
+
+void print_rev(char *s)
+{
+	char b;
+
+	b = strrev(s);
+	_putchar (b);
+}
