0a737486330edd75559d1b7fa92c107ee8e7d4ba
Iyanu456 <oyerindei13@gmail.com>
2022-09-20 16:14:04 -0700

    default

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * _strcpy - prints a string
+ * @dest: string destination
+ * @src: string source
+ * Description: prints a string to std output
+ */
+
+char _strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
