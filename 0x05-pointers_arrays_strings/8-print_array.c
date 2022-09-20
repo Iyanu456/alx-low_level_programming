aff9eac09a1f7f5320e945292718d5fad5f1919a
Iyanu456 <oyerindei13@gmail.com>
2022-09-20 15:22:05 -0700

    default

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,28 +4,16 @@
 /**
  * print_array -reverses a string
  * @a: array
-0;276;0c * @n: integer
+ * @n: integer
  * Description: prints items in an array;
  */
 
 void print_array(int *a, int n)
 {
 	int i;
-
-	if (n > 0)
+	
+	for (i = 0; i <= n; i++)
 	{
-	for (i = 0; i < n; i++)
-	{
-		if (i == n - 1)
-		{
-			printf("%d\n", a[i]);
-		}
-		else
-		{
-			printf("%d", a[i]);
-			printf(",");
-			printf(" ");
-		}
-	}
+		printf("%d, ", a[i]);
 	}
 }
