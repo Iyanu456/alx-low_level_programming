7d49c6bb51f98dfd0af79a7be2acbe77d7e3b901
Iyanu456 <oyerindei13@gmail.com>
2022-09-19 17:12:21 -0700

    default

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * puts_half - prints a string
+ * @str: integer
+ * Description: prints a string to std output
+ */
+
+void puts_half(char *str)
+{
+	int n, i, k;
+
+	n = (strlen(str) - 1);
+	k = (strlen(s) / 2);
+
+	for (i = 0; i < k; i++);
+	{
+		_putchar (str[(n - i)]);
+	}
+	_putchar ('\n');
+}
