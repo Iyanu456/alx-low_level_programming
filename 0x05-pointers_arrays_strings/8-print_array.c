7eefab8012bd2318dc9b02c90602dd61333fe5de
Iyanu456 <oyerindei13@gmail.com>
2022-09-20 06:28:47 -0700

    default

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_array -reverses a string
+ * @a: array
+ * @n: integer
+ * Description: prints items in an array;
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= 5; i++)
+	{
+		_putchar (a[i]);
+		_putchar (',');
+		_putchar (' ');
+	}
+	_putchar ('\n');
+}
