1a58efb267f85057820ec9d47faa61bd38a90488
Iyanu456 <oyerindei13@gmail.com>
2022-09-21 14:45:09 -0700

    default message

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <string.h>
 /**
- * reverse_array -reverses a string
+0;276;0c * reverse_array -reverses a string
  * @a: an array
  * @n: number of array element
  * Description: prints a string to std output
@@ -11,10 +11,17 @@ void reverse_array(int *a, int n)
 {
 	int i, k;
 
+	if (n >= 0)
+	{
 	for (i = 0; i < n; i++)
 	{
 		k = a[i];
 		a[i] = a[(n - 1 - i)];
 		a[(n - 1 - i)] = k;
+	}
+	}
+	else
+	{
+		_putchar ('\n');
 	}
 }
