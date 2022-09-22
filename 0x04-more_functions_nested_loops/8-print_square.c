36bfa11d43651f707fd3ce447ff2f2d570f431a5
Iyanu456 <oyerindei13@gmail.com>
2022-09-21 17:36:34 -0700

    default message

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -15,9 +15,13 @@ void print_square(int size)
 	{
 		for (i = 0; i < size; i++)
 		{
-			_putchar('#');
+			_putchar ('#');
 		}
-		_putchar('\n');
+		_putchar ('\n');
 	}
+	}
+	else
+	{
+		_putchar ('\n');
 	}
 }
