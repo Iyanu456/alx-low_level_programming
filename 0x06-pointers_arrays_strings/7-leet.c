422f218682cc65bc4fdafa0a8b10cbe3d71a559a
Iyanu456 <oyerindei13@gmail.com>
2022-09-23 17:50:26 -0700

    default message

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,28 +12,13 @@ char *leet(char *c)
 {
 	int i, j;
 
+	char leet[8] = { 'O', 'L', '?', 'E', 'A', '?', '?', 'T'}
 	j = strlen(c) - 1;
-	for (i = 0; i <= j; i++)
+	for (i = 0; i <= 7; i++)
 	{
-		if (c[i] == 'a' || c[i] == 'A')
+		if (c[i] == leet[i] || c[i] == leet[i] + 32)
 		{
-			c[i] = '4';
-		}
-		else if (c[i] == 'e' || c[i] == 'E')
-		{
-			c[i] = '3';
-		}
-		else if (c[i] == 'o' || c[i] == 'O')
-		{
-			c[i] = '0';
-		}
-		else if (c[i] == 't' || c[i] == 'T')
-		{
-			c[i] = '7';
-		}
-		else if (c[i] == 'l' || c[i] == 'L')
-		{
-			c[i] = '1';
+			c[i] = i;
 		}
 		else
 		{
