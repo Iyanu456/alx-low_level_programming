78134f7a2d9ed797d35e311164169ee6eab44796
Iyanu456 <oyerindei13@gmail.com>
2022-09-26 10:12:11 -0700

    default message

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,7 +6,7 @@
  * @s: a char type
  * @b: a char type
  * @n: an unasigned integer
- * Description: fills the first n bytes of the memory area pointed to by s with the constant byte b
+ * Description: fills the first n bytes of the memory area pointed to by s
  * Return: *s
  */
 
@@ -18,7 +18,7 @@ char *_memset(char *s, char b, unsigned int n)
 	j = n;
 	for (i = 0; i < j; i++)
 	{
-	memset(s + i, b, 1*sizeof(char));
+	memset(s + i, b, 1 * sizeof(char));
 	}
 	return (s);
 }
