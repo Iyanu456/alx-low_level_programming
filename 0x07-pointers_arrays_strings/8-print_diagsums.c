ee506bac5fc2c65d846e5bad56c880f4dfd84131
Iyanu456 <oyerindei13@gmail.com>
2022-09-27 03:36:41 -0700

    default message

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,28 +10,24 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, *j, k, *n, current_sum1, current_sum2;
+	int i, k, current_sum1, current_sum2;
 
-	j = 0;
-	n = 0;
-	*j = size;
 	current_sum1 = 0;
 	current_sum2 = 0;
 	for (i = 0; i < size; i++)
 	{
-		for (k = 0; k < size - 1; k++)
+		for (k = 0; k <= size; k++)
 		{
-		if (k != size - 1)
+		if (k != size)
 		{
-			current_sum1 += (a[i][j - k]);
-			*n = k;
-			current_sum2 += (a[i][n]);
+			current_sum1 += a[(size - 1 - k)];
+			current_sum2 += a[i];
 		}
 		else
 		{
-			printf("%d, %d", current_sum1, current_sum2);
 			break;
 		}
 		}
 	}
+	printf("%d, %d", current_sum1, current_sum2);
 }
