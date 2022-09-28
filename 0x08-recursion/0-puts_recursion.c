9464eac68215217c4351ef4e2a4db2b9e157a0bc
Iyanu456 <oyerindei13@gmail.com>
2022-09-28 16:57:41 -0700

    default message

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -12,11 +12,10 @@ void _puts_recursion(char *s)
 	int i;
 
 	i = 0;
-	_putchar(s[i]);
-	i++;
-	if (i == '\0')
+	_putchar(s);
+	s++;
+	if (i != \0)
 	{
-		_putchar('\n');
+		_puts_recursion(s);
 	}
-	_puts_recursion(s);
 }
