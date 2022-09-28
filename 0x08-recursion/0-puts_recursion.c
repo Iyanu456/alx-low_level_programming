5c5d64bc8f84b3ac9ebb16471b4821b8b14e4fea
Iyanu456 <oyerindei13@gmail.com>
2022-09-28 16:44:28 -0700

    default message

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a atring through recursion
+ * @s: a string
+ * Description: prints a string
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+	_putchar(s[i]);
+	i++;
+	if (i != '\0')
+	{
+		_puts_recursion(s);
+	}
+	_putchar('\n');
+}
