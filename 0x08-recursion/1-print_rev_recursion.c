e0f4809f2b86b7f70c6f0355d9d23275e32cdcb9
Iyanu456 <oyerindei13@gmail.com>
2022-09-29 08:23:18 -0700

    default message

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "string.h"
+#include <stdio.h>
+/**
+ * _print_rev_recursion - prints a atring through recursion
+ * @s: a string
+ * Description: prints a string
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	int i, j, k;
+
+	i = 0;
+	j = strlen(s);
+	if (i >= 0)
+	{
+		_putchar (s[j-i]);
+		i++;
+	}
+	_print_rev_recursion(s);
+}
