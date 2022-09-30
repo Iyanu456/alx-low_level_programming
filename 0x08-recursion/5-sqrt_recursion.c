f4c602d67cef096093a1348478628dc68368a615
Iyanu456 <oyerindei13@gmail.com>
2022-09-29 17:34:16 -0700

    default message

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,5 +10,42 @@
 
 int _sqrt_recursion(int n)
 {
-	return (sqrt(n));
+	int sqrt_search(int start, int end, int value)
+	{
+		if (end * end == value)
+		{
+			return (end);
+		}
+		else if (end * end > value)
+		{
+		int mid, m_square;
+
+		mid = (start + end) / 2;
+		m_square = mid * mid;
+		if (m_square == value)
+		{
+			return (mid);
+		}
+		else if (m_square - value <= 0.001)
+		{
+			return (mid);
+		}
+		else if (m_square < value)
+		{
+			return (sqrt_search(start, mid + 1, value));
+		}
+		else if (m_square > value)
+		{
+			return (sqrt_search(mid - 1, end, value));
+		}
+		}
+		else if (end * end < n)
+		{
+			sqrt_search(start + 1, end + 1, value);
+		}
+	}
+	int i;
+
+	i = 0;
+	return (sqrt_search(i, i + 1, n));
 }
