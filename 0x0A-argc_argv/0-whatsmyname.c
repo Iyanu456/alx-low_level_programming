ad4d6ba1b4ec26c9a33eb175b7f22663dd838a74
Iyanu456 <oyerindei13@gmail.com>
2022-09-30 16:26:52 -0700

    default message

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - calls main function
+ * @argc: argument count
+ * @argv: argument variable
+ * Description: function that prints its name
+ * Return: 1
+ */
+
+int main(int argc, char *argv)
+{
+	_putchar (argv[0]);
+	return (0);
+}
