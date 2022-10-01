5acee25b407d248e651617d42764b39b75d639d9
Iyanu456 <oyerindei13@gmail.com>
2022-09-30 17:42:37 -0700

    default message

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -15,10 +15,7 @@ int main(int argc, char *argv[])
 
 	for (i = 0; i < argc; i++)
 	{
-		for (n = 0; n < argc; n++)
-		{
-			printf("%s\n", argv[n]);
-		}
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
