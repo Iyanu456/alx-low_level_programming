b739cf95f7abc1a142ee102c01cd317bb8ae1012
Iyanu456 <oyerindei13@gmail.com>
2022-09-30 17:20:37 -0700

    default message

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - calls main function
+ * @argc: argument count
+ * @argv: argument variable
+ * Description: function that prints the total number of ccommand line arguments
+ * Return: 1
+ */
+
+int main(int argc,__attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
