323034007947948c594c7f7887c4027979797770
Iyanu456 <oyerindei13@gmail.com>
2022-10-03 05:52:23 -0700

    default message

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * create_array - creates an array of given size
+ * @size: size of array
+ * @c: a char
+ * Description: creates an array of gven size
+ * Return: char
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	if (size != 0)
+	{
+		char *a;
+
+		int i;
+
+		a = malloc(sizeof(a) * size);
+	}
+}
