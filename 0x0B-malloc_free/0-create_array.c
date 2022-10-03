9921ab0dab65d7fc0ec51635d84521dc5012b8fd
Iyanu456 <oyerindei13@gmail.com>
2022-10-03 16:06:53 -0700

    default message

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,11 +12,10 @@ char *create_array(unsigned int size, char c)
 {
 	char *a;
 
+	a = malloc(sizeof(char) * size);
 	if (size != 0)
 	{
 		unsigned int i;
-
-		a = malloc(sizeof(char) * size);
 
 		for (i = 0; i < size; i++)
 		{
