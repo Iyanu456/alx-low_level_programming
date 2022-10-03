24dc833a209f582717f56f64411c1e758975f753
Iyanu456 <oyerindei13@gmail.com>
2022-10-02 17:47:19 -0700

    default message

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints a string
+ * @str: integer
+ * Description: prints a string to std output
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
