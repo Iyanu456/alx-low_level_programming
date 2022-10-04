70341d6534a9b32191290fab462064475ff336f4
Iyanu456 <oyerindei13@gmail.com>
2022-10-04 12:13:08 -0700

    default message

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -23,7 +23,7 @@ char *_strdup(char *str)
 	{
 		i++;
 	}
-	str2 = (char *)malloc(sizeof(char) * (i +  1));
+	str2 = malloc(sizeof(char) * (i +  1));
 
 	while (str[len] != '\0')
 	{
@@ -36,6 +36,7 @@ char *_strdup(char *str)
 	}
 	else
 	{
+		str[len] = '\0';
 		return (str);
 	}
 }
