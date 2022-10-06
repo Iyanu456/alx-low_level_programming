9ec0faf4f10a8f3b2f516abcde22b58d0d520815
Iyanu456 <oyerindei13@gmail.com>
2022-10-06 16:06:01 -0700

    default message

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,6 +13,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *arr;
 
+	int i;
+
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
@@ -21,5 +23,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (arr == NULL)
 		return (NULL);
 
+	for(i = 0; i < nmemb; i++)
+	{
+		arr[i] = nmemb;
+	}
 	return (arr);
 }
