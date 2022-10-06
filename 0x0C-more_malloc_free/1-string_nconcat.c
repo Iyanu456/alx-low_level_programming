155300aa3210b184931637923ac91c4e4dcd7a03
Iyanu456 <oyerindei13@gmail.com>
2022-10-06 13:31:19 -0700

    default message

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -36,7 +36,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		n = length2;
 	}
 
-	str = malloc(sizeof(char) * (length1 + n + )1);
+	str = malloc(sizeof(char) * (length1 + n) + 1);
 
 	if (str == NULL)
 	{
