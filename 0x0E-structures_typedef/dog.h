a736f590b91d527ea393c622172b610449c3c8bd
Iyanu456 <oyerindei13@gmail.com>
2022-10-10 10:18:44 -0700

    default message

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+/**
+ * dog - dog struct
+ * @name: namee of dog
+ * @owner: dog owner
+ * @age: dog's age
+ * Description:construct a dog
+ */
+
+struct dog
+{
+	char *name;
+
+	char *owner;
+
+	float age;
+}
