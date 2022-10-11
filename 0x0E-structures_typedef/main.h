9f9458df05f77c60c4ae7c193804588b31841a8b
Iyanu456 <oyerindei13@gmail.com>
2022-10-10 17:22:05 -0700

    default message

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -1,12 +1,4 @@
 #ifndef MAIN_H
 #define MAIN_H
-void init_dog(struct dog *d, char name, float age, char owner);
-struct dog
-{
-	char *name;
-
-	char *owner;
-
-	float age;
-};
+void init_dog(struct dog *d, char name, float age, char *owner);
 #endif
