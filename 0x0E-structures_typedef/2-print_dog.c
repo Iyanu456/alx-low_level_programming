91f6a86d561f42839825c61e888e2c4d65d0fd06
Iyanu456 <oyerindei13@gmail.com>
2022-10-10 18:19:54 -0700

    default message

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * print_dog - initilizes a dog
+ * @d: new dog struct
+ * Description: initializes a dog
+ */
+void init_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		printf("Name: (nil)");
+	else
+		printf("Name: %d\n", d->name);
+
+	if (d->age == NULL)
+		printf("Age: (nil)");
+	else
+		printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("Owner: (nil)");
+	else
+		printf("Owner: %d\n", d->owner);
+}
