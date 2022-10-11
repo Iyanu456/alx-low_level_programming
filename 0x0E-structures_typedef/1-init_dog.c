d9d44c1caf8714edb1a79b2778e0e32c9778fd95
Iyanu456 <oyerindei13@gmail.com>
2022-10-10 18:09:41 -0700

    default message

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,13 @@
 #include <stdlib.h>
 #include "dog.h"
+/**
+ * init_dog - initilizes a dog
+ * @d: new dog struct
+ * @name: name of dog
+ * @age: dog's age
+ * @owner: dog owner
+ * Description: initializes a dog
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
