2d21df378ce7118b3df01c2af8fcc3918c7266dc
Iyanu456 <oyerindei13@gmail.com>
2022-10-13 19:39:21 -0700

    default message

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,14 +9,16 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	int i, sum = 0;
+	unsigned int i;
 
+	int sum = 0;
+
+	va_list item;
 	if (n)
 	{
 	if (n == 0)
 		return (0);
 
-	va_list item;
 	va_start(item, n);
 
 	for (i = 0; i < n; i++)
