c6691b7ef3cce0bc88b53a8b4410a6dd605964bf
Iyanu456 <oyerindei13@gmail.com>
2022-10-14 16:46:59 -0700

    default message

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,4 +11,17 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+	if (*argv[2] != '+' || '-' || '*' || '/' || '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*argv[2] == '/' || '%') && *argv[3] == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	result = get_op_func(argv[1], argv[3]);
+	printf("%d", result);
+	return (0);
 }
