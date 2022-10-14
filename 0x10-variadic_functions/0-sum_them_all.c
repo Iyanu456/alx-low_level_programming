1dbfd44d61d69c8d6eaf14ef6cf067bf09debe93
Iyanu456 <oyerindei13@gmail.com>
2022-10-13 18:04:37 -0700

    default message

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,7 +10,7 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list item;
 
-	unsigned int i, sum = 0;
+	int i, sum = n;
 
 	if (n)
 	{
@@ -19,7 +19,7 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(item, n);
 
-	for (i = n; i >= 0; i = va_arg(item, unsigned int))
+	for (i = n; i >= 0; i = va_arg(item, int))
 	{
 		sum += i;
 	}
