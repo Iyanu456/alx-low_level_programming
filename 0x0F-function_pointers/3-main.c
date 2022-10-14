c25f3a7d876fcf7a9bbe713085253a6519569f3d
Iyanu456 <oyerindei13@gmail.com>
2022-10-14 16:33:34 -0700

    default message

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include "3-calc.h"
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
 	int result;
 
-	if (arg c != 4)
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
