0f39c1bb97d51b3b3ddac05b0bbc2da0c9359f5a
Iyanu456 <oyerindei13@gmail.com>
2022-10-20 16:46:55 -0700

    default message

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,13 +7,17 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *new_ptr;
+
 	list_t *new_node;
 
+	new_ptr = *head;
 	if (head == NULL || str == NULL)
 		return (NULL);
 
 	new_node = malloc(sizeof(list_t));
 	new_node->str = strdup(str);
 	new_node->next = *head;
+	*head = new_ptr;
 	return (new_node);
 }
