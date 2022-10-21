07d3e847bf9a55292a5426bb042455663e52f751
Iyanu456 <oyerindei13@gmail.com>
2022-10-20 18:25:30 -0700

    default message

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -18,9 +18,9 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->str = strdup(str);
 	while (str[len] != '\0')
 		len++;
-	new_node->len = NULL;
+	new_node->len = len;
 	new_node->next = NULL;
-	if (head->next == NULL)
+	if (*head == NULL)
 		*head = new_node;
 	else
 	{
@@ -28,7 +28,8 @@ list_t *add_node_end(list_t **head, const char *str)
 		while (temp->next != NULL)
 		{
 			temp = temp->next;
-			temp->next = new_ptr;
+			temp->next = new_node;
 		}
+	}
 		return (*head);
 }
