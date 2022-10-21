58c185b1442d1c4bb9aab1bdc9f17dddac6b3369
Iyanu456 <oyerindei13@gmail.com>
2022-10-21 11:50:04 -0700

    default message

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * add_node - creates a new node
+ * @head: head node pointer
+ * @str: string
+ * Description: creates a new node
+ * Return: size
+ */
+void free_list(list_t *head)
+{
+	if (head == NULL)
+		return;
+
+	list_t *curr_node, *next_node;
+	if (head->next == NULL)
+		free(head);
+	else
+	{
+		curr_node = head;
+		free(head);
+		while (curr_node->next != NULL)
+		{
+			curr_node = curr_node->next;
+			next_node  = curr_node;
+			free(curr_node);
+			next_node = next_node->next;
+			curr_node = next_node;
+			free(next_node);
+		}
+	}
+}
