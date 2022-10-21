409a4ba578a97aa048b103ebd5ae56fa15b437cc
Iyanu456 <oyerindei13@gmail.com>
2022-10-21 04:52:04 -0700

    default message

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,14 +19,14 @@ void print_dog(struct dog *d)
 	owner = d->owner;
 	age = d->age;
 
-	if (d == 0)
+	if (d == NULL)
                 return;
 
-        if (name == NULL)
+        if (d->name == NULL)
                 name = "(nil)";
 	else
 		name = d->name;
- 	if (owner == NULL)
+ 	if (d->owner == NULL)
 		owner = "(nil)";
 	else
 		owner = d->owner;
@@ -34,7 +34,7 @@ void print_dog(struct dog *d)
 	printf("Name: %s\n", name);
     	printf("Owner: %s\n", owner);
 
-	if (age >=0)
+	if (d->age >=0)
 		printf("Age: (nil)");
 	else
 		printf("Age: %f\n", age);
