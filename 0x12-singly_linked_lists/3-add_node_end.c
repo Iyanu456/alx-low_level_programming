99342abf4c2497b854d97c7f79426f661c81308a
Iyanu456 <oyerindei13@gmail.com>
2022-10-21 06:08:55 -0700

    default message

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -23,7 +23,8 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 
 	dup = strdup(str);
-
+	if (dup == NULL)
+		return (NULL);
 	new_node->str = dup;
 
 	for (len = 0; dup[len] != '\0'; len++)
