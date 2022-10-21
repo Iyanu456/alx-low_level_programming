20a9dc8549fba19b2075ca05621895c549a4489a
Iyanu456 <oyerindei13@gmail.com>
2022-10-21 06:03:15 -0700

    default message

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -16,7 +16,7 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	new_node = malloc(sizeof(list_t));
 
-	if (new_node == NULL || str == NULL)
+	if (new_node == NULL)
 	{
 		free(new_node);
 		return (NULL);
