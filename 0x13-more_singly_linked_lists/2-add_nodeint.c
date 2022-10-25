8f476c5087b46d6b21a4dd314a9dc4f021a80e4c
Iyanu456 <oyerindei13@gmail.com>
2022-10-25 12:54:49 -0700

    default message

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,7 +13,7 @@ listint_t *add_node(listint_t **head, const int n)
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL || n == NULL)
 		return (NULL);
-	new_node->len = n;
+	new_node->n = n;
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
