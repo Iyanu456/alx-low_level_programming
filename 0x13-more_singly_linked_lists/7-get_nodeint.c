a3e3ac5d84d9e6d3069a8b18adc7ae7f6011c69d
Iyanu456 <oyerindei13@gmail.com>
2022-10-26 05:22:22 -0700

    default message

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * pop_listint - frees a node list
+ * @head: head node pointer
+ * Description: frees a node list
+ * Return: void
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *temp;
+
+	int i = 0;
+
+	if (head == NULL)
+		return (NULL);
+
+	temp = head;
+	while (i < index)
+	{
+		temp = temp->next;
+
+		if  (temp = NULL)
+			return (NULL);
+		i++;
+	}
+
+	return (temp);
+}
