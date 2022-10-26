d72e0a47a6bebc6e3d44760297b4396c1bea0b4c
Iyanu456 <oyerindei13@gmail.com>
2022-10-26 03:49:27 -0700

    default message

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,11 +9,14 @@ int pop_listint(listint_t **head)
 {
 	listint_t *temp;
 
+	int n;
+
 	if ((*head)->next == NULL)
 		return (0);
 
 	temp = (*head)->next;
+	n = (*head)->n;
 	free(*head);
 	*head = temp->next;
-	return ((*head)->n);
+	return (n);
 }
