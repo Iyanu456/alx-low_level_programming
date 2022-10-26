c3a57666dce3a233846f2117e0c3bab432d6c3d0
Iyanu456 <oyerindei13@gmail.com>
2022-10-25 17:12:10 -0700

    default message

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a node list
+ * @head: head node pointer
+ * Description: frees a node list
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = *head->next;
+		free(*head);
+		*head = temp;
+	}
+
+	*head = NULL;
+}
