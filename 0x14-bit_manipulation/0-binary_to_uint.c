1c7f2fecd96445e008d2b3346d6a035d0b0815b8
Iyanu456 <oyerindei13@gmail.com>
2022-10-27 14:28:30 -0700

    default message

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,17 +3,18 @@ unsigned int binary_to_uint(const char *b)
 {
 	int i = 0;
 
-	unsigned int result = 0;
+	unsigned int result = 0, converted = 0;
 
 	if (b == NULL)
 		return (0);
 
-	while (b[i] != '\0')
+	while (b)
 	{
 		if (b[i] > 1)
 			return (0);
 
-		result += (b[i] * (2 ^ i));
+		converted = b[i] * (2 ^ i);
+		result += converted;
 		i++;
 	}
 
