bdfd79dcc7c7ac8dff5d7fdc53903a9a31489232
Iyanu456 <oyerindei13@gmail.com>
2022-10-27 14:18:29 -0700

    default message

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,20 @@
+#include "main.h"
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+
+	unsigned int result = 0;
+
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != 1 || 0)
+			return (0);
+
+		result += b[i] * (2 ^ i);
+	}
+
+	return (result);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,5 +3,5 @@
 #include <stdlib.h>
 
 int _putchar(char c);
-unsigned int binary_to_unit(const char *b);
+unsigned int binary_to_uint(const char *b);
 #endif
