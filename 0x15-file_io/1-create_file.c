4e65757b71329349664bdad8b187442478c757cb
Iyanu456 <oyerindei13@gmail.com>
2022-10-31 15:56:55 -0700

    default message

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and writes it to sdtout
+ * @filename: filename
+ * @letters: letters
+ * Description: reads a text file and writes it to stdout
+ * Return: textfile
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int opened, i, wrote, closing;
+
+	char *buffer;
+
+	if (filename == NULL)
+		return (-1);
+
+	opened = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+
+	if (opened == -1)
+		return (-1);
+	if (text_content == NULL)
+		i = 0;
+	else
+	{
+		i = 0;
+		while (text_content[i] != '\0')
+			i++;
+	}
+
+	wrote = write(opened, text_content, i);
+
+	if (wrote == -1)
+		return (-1);
+
+	close(opened);
+	return (wrote);
+}
