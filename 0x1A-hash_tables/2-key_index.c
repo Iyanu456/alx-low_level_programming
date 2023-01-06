13a57b3848252d4104d5bfe4ca88fa815def128f
Iyanu456 <oyerindei13@gmail.com>
2023-01-06 04:57:53 -0800

    added 2-key_index.c

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+/**
+ * key_index - finds the index of a key
+ * @key: key
+ * @size: size of hash table
+ * Description: finds the index of a given key
+ * Return: key index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int i;
+	const unsigned char *str = key;
+
+	for (i = 0; i < size, i++)
+	{
+	}
+}
