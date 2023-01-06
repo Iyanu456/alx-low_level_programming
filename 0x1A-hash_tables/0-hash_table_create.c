4491c8d0dd706b2dff32da365d871c6c7bc4c550
Iyanu456 <oyerindei13@gmail.com>
2023-01-06 14:12:12 +0100

    new_commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,6 +1,6 @@
 #include "hash_tables.h"
 /**
- * hash_table_create: creates an hash_table
+ * hash_table_create - creates an hash_table
  * @size: size of hash_table
  * Description: function thats creates a hash table
  * Return: pointer to hash_table
@@ -9,7 +9,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
-	hash_table_t *ht; 
+	hash_table_t *ht;
 
 	ht = malloc(sizeof(hash_table_t));
 
