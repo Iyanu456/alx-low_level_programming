d62ec9814c5ba4f1a7738546749222ff2b49df7f
Iyanu456 <oyerindei13@gmail.com>
2023-01-07 00:52:05 +0100

    default message

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -8,10 +8,5 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int i;
-	const unsigned char *str = key;
-
-	for (i = 0; i < size, i++)
-	{
-	}
+	return (hash_djb2(key) % size);
 }
