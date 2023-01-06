e6bde69b2c8d6b3c3fa7fea91e45313771903279
Iyanu456 <oyerindei13@gmail.com>
2023-01-06 09:30:38 +0100

    default message

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,8 @@
+#include <hash_tables.h>
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
+	hash_table->size = size;
+	return (hash_table);
+}
