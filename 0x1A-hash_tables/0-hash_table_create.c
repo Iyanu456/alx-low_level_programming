11dba824bd678ba53ea20d74c28adb93154cb3d3
Iyanu456 <oyerindei13@gmail.com>
2023-01-06 10:08:23 +0100

    default message

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,7 +8,19 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
-	hash_table->size = size;
-	return (hash_table);
+	unsigned long int i;
+	hash_table_t *ht = malloc(sizeof(hash_table_t));
+	
+	if (ht == NULL)
+		return (NULL);
+	
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+
+	for (i = 0; i < size; i++)
+	{
+		ht->array[i] = NULL;
+	}
+	
+	return (ht);
 }
