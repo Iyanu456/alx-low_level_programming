b251a2fccaa25947d7e45cc1f63c89ca4294126d
Iyanu456 <oyerindei13@gmail.com>
2023-01-31 07:14:16 -0800

    default

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strpbrk - prints a string
+ * @s: string
+ * @accept: string
+ * Description: prints a string to std output
+ * Return: size_t
+ */
+char *strpbrk(const char *s, const char *accept)
+{
+}
---
+++ backup_static_lib/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strcmp - concatenates two strings
+ * Description: concatenates two strings
+ * @s1: destination string
+ * @s2: source string
+ * Return: string
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = strcmp(s1, s2);
+	return (i);
+}
