bb0b763532e277ebe4c4b66a3c22c0af49f8f861
Iyanu456 <oyerindei13@gmail.com>
2023-02-25 00:47:07 +0100

    default message

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,9 +7,6 @@ def island_perimeter(grid):
 
     perimeter = 0
 
-    if grid is None:
-        return 0
-
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
